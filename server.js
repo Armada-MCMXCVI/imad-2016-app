b1906059e9a74405bf805cebce05f4672bf8d25b
Armada-MCMXCVI <chetan_dhanjal@yahoo.com>
2016-09-23 20:14:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.get('/article-one',function (req,res) {
 });
 
 app.get('/article-two',function (req,res) {
-   res.send("Article 2 called for.");
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function (req,res) {
-   res.send("Article 3 calledd for."); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
