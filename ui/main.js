8fa9f23c641600742b2dcaf417526d1b6d85448e
Armada-MCMXCVI <chetan_dhanjal@yahoo.com>
2016-09-24 15:55:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,11 @@ element.innerHtml = 'Kya Haal Chaal';
 
 //Change color
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft +5;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-img.style.marginLeft = '100px';
+var interval = setInterval(moveRight,100);
 }
